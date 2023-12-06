250853006117fcbfe091e9acb29754c47be9cf39
safrthaktyagi354932 <sarthaktyagi354932@gmail.com>
2023-12-07 00:35:31 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: Material(
+      
+        child: Center(
+          
+          child: Container(
+            child: const Text("welcome helo",style: TextStyle(color: Color.fromARGB(255, 47, 4, 4)),),
+            
+            
+          ),
+        
+        ),
+        
+      ),
+    );
+
+    
+  }
+}
